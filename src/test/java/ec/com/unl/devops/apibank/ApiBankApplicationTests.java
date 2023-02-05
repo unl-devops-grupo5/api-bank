e7d85cb0b58039e9199931c24b2c0117e8da4410
Pedro <fernandoaponte1103@gmail.com>
2023-02-05 02:32:07 -0500

    Inicio proyecto

---
+++ src/test/java/ec/com/unl/devops/apibank/ApiBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.unl.devops.apibank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
